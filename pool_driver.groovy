db6d70e806b832ff895343a38bb69a69fbc4695e
Ryan Sullivan <ryan.sullivan@charter.com>
2019-07-09 15:03:06 -0400

    testing code

--- pool_driver.groovy
+++ pool_driver.groovy
@@ -20,11 +20,10 @@
  */
 
 metadata {
-    definition (name: "Custom Pool Driver", namespace: "Rerouted", author: "Ryan Sullivan", importUrl: "https://raw.githubusercontent.com/rerouted/Hubitat/master/Drivers/Pool/xxx.groovy") {
+    definition (name: "Custom Pool Driver", namespace: "Rerouted", author: "Ryan Sullivan", importUrl: "https://raw.githubusercontent.com/rerouted/hubitat-pool/master/pool_driver.groovy") {
         capability "Actuator"
         capability "Sensor"
         capability "TemperatureMeasurement"
-        capability "ThermostatHeatingSetpoint"
         
 
         command "poll"
@@ -47,7 +46,7 @@ metadata {
 			input "unitFormat", "enum", required: true, title: "Unit Format",  options: ["Imperial", "Metric"]
 	        input "pollIntervalLimit", "number", title: "Poll Interval Limit:", required: true, defaultValue: 1
             input "autoPoll", "bool", required: false, title: "Enable Auto Poll"
-            input "pollInterval", "enum", title: "Auto Poll Interval:", required: false, defaultValue: "5 Minutes", options: ["5 Minutes", "10 Minutes", "15 Minutes", "30 Minutes", "1 Hour", "3 Hours"]
+            input "pollInterval", "enum", title: "Auto Poll Interval:", required: false, defaultValue: "1 Minute", options: ["1 Minute", "5 Minutes", "10 Minutes", "15 Minutes"]
             input "logSet", "bool", title: "Enable Logging", required: true, defaultValue: false
         }
     }
@@ -55,16 +54,14 @@ metadata {
 
 def updated() {
     log.debug "updated called"
-   updateCheck()
     unschedule()
-    version()
     state.NumOfPolls = 0
     ForcePoll()
-    def pollIntervalCmd = (settings?.pollInterval ?: "5 Minutes").replace(" ", "")
+    def pollIntervalCmd = (settings?.pollInterval ?: "1 Minute").replace(" ", "")
     if(autoPoll)
         "runEvery${pollIntervalCmd}"(pollSchedule)
     
-     def changeOver = cutOff
+    def changeOver = cutOff
     schedule(changeOver, ResetPollCount)
     if(logSet){runIn(1800, logsOff)}
 }
@@ -116,8 +113,8 @@ def pollHandler1(resp, data) {
         if(logSet == true){log.debug "Response Data1 = $data"}		// log the data returned by pool controller
         if(logSet == false){log.info "Further logging disabled"}
             sendEvent(name: "pollsSinceReset", value: state.NumOfPolls)
-            sendEvent(name: "poolTemp", value: data.temperature.poolTemp, unit: ${$state.unit})
-			sendEvent(name: "airTemp", value: data.temperature.airTemp, unit: ${$state.unit})
+            sendEvent(name: "poolTemp", value: data.temperature.poolTemp, unit: state.unit)
+			sendEvent(name: "airTemp", value: data.temperature.airTemp, unit: state.unit)
 		state.lastPoll = now()
 
 	} else {
@@ -131,7 +128,7 @@ def poll1(){
     formatUnit()
     def params1 = [
         uri: "http://${serverAddress}:${serverPort}/all"
-     //   uri: "https://pool.rainseed.net:3000/v2/pws/observations/current?stationId=IDURHAM16&format=json&units=${state.unit}&apiKey=${apiKey}"
+     //   uri: "https://pool.rainseed.net:3000/temperature"
     ]
     asynchttpGet("pollHandler1", params1)   
 }
